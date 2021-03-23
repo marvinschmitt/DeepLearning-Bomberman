ffd771486c92a0b26a369e2694720bf7d07c000d
marvinschmitt <mail@marvin-schmitt.de>
2021-03-23 16:59:08 +0100

    [no lint] move live_preview to separate file

--- deep_bomber/eval_and_watch.py
+++ deep_bomber/eval_and_watch.py
@@ -3,16 +3,15 @@ import os
 import tensorflow as tf
 from tf_agents.environments import tf_py_environment
 
-from adapter.bomberman_adapter import BombermanEnvironment, BombermanGame
+from adapter.bomberman_adapter import BombermanEnvironment
 
 
 if __name__ == '__main__':
-    # eval_tf_env = tf_py_environment.TFPyEnvironment(BombermanEnvironment(mode='imitator', replay=True))
-    eval_tf_env = tf_py_environment.TFPyEnvironment(BombermanEnvironment(mode='fourchannel', live_preview=True))
+    eval_tf_env = tf_py_environment.TFPyEnvironment(BombermanEnvironment(mode='base', replay=True))
 
     time_step = eval_tf_env.reset()
 
-    policy = tf.saved_model.load("policy_400k")
+    policy = tf.saved_model.load("policy")
     policy_state = policy.get_initial_state(batch_size=eval_tf_env.batch_size)
 
     while not time_step.is_last():
@@ -20,8 +19,8 @@ if __name__ == '__main__':
         policy_state = policy_step.state
         time_step = eval_tf_env.step(int(policy_step.action))
 
-    # replay_files = os.listdir("replays")
-    # replay_file = sorted(replay_files)[-1]
+    replay_files = os.listdir("replays")
+    replay_file = sorted(replay_files)[-1]
 
-    # command = f"python main.py replay \"replays/{replay_file}\" --update-interval 0.03 --fps 60"
-    # os.system(command)
+    command = f"python main.py replay \"replays/{replay_file}\" --update-interval 0.03 --fps 60"
+    os.system(command)
---
+++ deep_bomber/live_preview.py
@@ -0,0 +1,20 @@
+import os
+
+import tensorflow as tf
+from tf_agents.environments import tf_py_environment
+
+from adapter.bomberman_adapter import BombermanEnvironment, BombermanGame
+
+
+if __name__ == '__main__':
+    eval_tf_env = tf_py_environment.TFPyEnvironment(BombermanEnvironment(mode='base', live_preview=True))
+
+    time_step = eval_tf_env.reset()
+
+    policy = tf.saved_model.load("policy_2")
+    policy_state = policy.get_initial_state(batch_size=eval_tf_env.batch_size)
+
+    while not time_step.is_last():
+        policy_step = policy.action(time_step, policy_state)
+        policy_state = policy_step.state
+        time_step = eval_tf_env.step(int(policy_step.action))
