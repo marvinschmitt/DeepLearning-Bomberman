44aa80992b374d727d5691b61623671f1983d2ed
marvinschmitt <mail@marvin-schmitt.de>
2021-03-23 15:05:43 +0100

    [no lint] adapt eval_and_watch to show live preview instead of recording

--- deep_bomber/eval_and_watch.py
+++ deep_bomber/eval_and_watch.py
@@ -7,21 +7,21 @@ from adapter.bomberman_adapter import BombermanEnvironment, BombermanGame
 
 
 if __name__ == '__main__':
-    eval_tf_env = tf_py_environment.TFPyEnvironment(BombermanEnvironment(mode='imitator', replay=True))
+    # eval_tf_env = tf_py_environment.TFPyEnvironment(BombermanEnvironment(mode='imitator', replay=True))
+    eval_tf_env = tf_py_environment.TFPyEnvironment(BombermanEnvironment(mode='fourchannel', live_preview=True))
 
     time_step = eval_tf_env.reset()
 
-    policy = tf.saved_model.load("policy_imitator")
+    policy = tf.saved_model.load("policy_400k")
     policy_state = policy.get_initial_state(batch_size=eval_tf_env.batch_size)
 
     while not time_step.is_last():
         policy_step = policy.action(time_step, policy_state)
         policy_state = policy_step.state
-        print(BombermanGame.ACTIONS[int(policy_step.action)])
         time_step = eval_tf_env.step(int(policy_step.action))
 
-    replay_files = os.listdir("replays")
-    replay_file = sorted(replay_files)[-1]
+    # replay_files = os.listdir("replays")
+    # replay_file = sorted(replay_files)[-1]
 
-    command = f"python main.py replay \"replays/{replay_file}\" --update-interval 0.03 --fps 60"
-    os.system(command)
+    # command = f"python main.py replay \"replays/{replay_file}\" --update-interval 0.03 --fps 60"
+    # os.system(command)
